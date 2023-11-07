565ccdbc3805be566e9b04c99ddfb59efc5cacbe
Zark_D <vargascardona12@gmail.com>
2023-11-07 09:44:29 -0500

    Implementadas vistas de registro para cuentas y contactos

--- vistas/vistaAcceso.py
+++ vistas/vistaAcceso.py
@@ -9,24 +9,48 @@ def cerrar_vista():
     dpg.delete_item("user_view")
     dpg.configure_item("login_window", show=True)
 
+def crear_notificacion(mensaje: str):
+       with dpg.window(label="Alerta", modal=True, show=True, tag="notificacion", pos=(620,300)):
+           dpg.add_text(mensaje)
+           dpg.add_separator()
+           with dpg.group(horizontal=True):
+              dpg.add_button(label="OK", width=75, callback=lambda: dpg.delete_item("notificacion"))
+
+def registro_cuenta():
+    dpg.configure_item("login_window", show=False)
+    with dpg.window(tag="register_window", label="Registro de cuenta", no_close=True, no_collapse=True, no_resize=True, width=281, height=250, pos=(620,300)):
+      dpg.add_text("Cedúla")
+      dpg.add_input_text(tag="cedula", hint="Cedúla", width=265)
+      dpg.add_text("Nombre Completo")
+      dpg.add_input_text(tag="full_name", hint="Nombre Completo", width=265)
+      dpg.add_text("Nombre de Usuario")
+      dpg.add_input_text(tag="user_registration", hint="Usuario", width=265)
+      dpg.add_text("Contraseña")
+      dpg.add_input_text(tag="pass_registration", hint="Contraseña", password=True, width=265)
+      dpg.add_spacer(height=2)
+      dpg.add_button(label="Registrar Cuenta", track_offset=0.5, callback=lambda: (dpg.delete_item("register_window"), dpg.configure_item("login_window", show=True)))
+
+
+
 def iniciar_sesion():
     usuario = dpg.get_value("user")
     contrasenia = dpg.get_value("pass")
     cuenta = dao_cuenta.obtener_cuenta(usuario)
+
     if cuenta == None:
-        print("No se encontró el usuario")
+        crear_notificacion("Cuenta no encontrada")
         return
+        
     if cuenta.contrasenia != contrasenia:
-        print("Contraseña incorrecta")
+        crear_notificacion("Contraseña Incorrecta")
         return
     
-    with dpg.window(tag="user_view", no_close=True, no_collapse=True, no_resize=True, width=300, height=300):
-        dpg.configure_item("login_window", show=False)
-        dpg.add_text(f"Usuario: {cuenta.usuario}")
-
+    dpg.configure_item("login_window", show=False)
+    with dpg.window(label=f"Agenda de {cuenta.usuario}", tag="user_view", no_close=True, no_collapse=True, no_resize=True, width=590, height=300, pos=(620,300)):
+       with dpg.group(horizontal=True):
         with dpg.table(header_row=True, row_background=True,
                    borders_innerH=True, borders_outerH=True, borders_innerV=True,
-                   borders_outerV=True):
+                   borders_outerV=True, width=300, height=200):
             dpg.add_table_column(label="Nombre")
             dpg.add_table_column(label="Teléfono")
             
@@ -37,10 +61,21 @@ def iniciar_sesion():
                     dpg.add_text(f"{contacto.nombre_completo}")
                     dpg.add_text(f"{contacto.telefono}")
 
-        dpg.add_spacer(height=10)
-        dpg.add_button(label="Cerrar Sesión", callback=cerrar_vista)
+        with dpg.group(horizontal=False):
+          dpg.add_text("Nombre Contacto")
+          dpg.add_input_text(tag="contact_name", hint="Nombre Contacto", width=265)
+          dpg.add_text("Numero Telefonico")
+          dpg.add_input_text(tag="telephone_number", hint="Numero Telefonico", width=265)
+          dpg.add_spacer(height=2)
+          with dpg.group(horizontal=True):
+           dpg.add_button(label="Registrar", callback=cerrar_vista)
+           dpg.add_button(label="Editar", callback=cerrar_vista)
+           dpg.add_button(label="Eliminar Contacto", callback=cerrar_vista)
 
-with dpg.window(tag="login_window", label="Iniciar Sesión", no_close=True, no_collapse=True, no_resize=True, width=280, height=155):
+       dpg.add_spacer(height=10)
+       dpg.add_button(label="Cerrar Sesión", callback=cerrar_vista)
+
+with dpg.window(tag="login_window", label="Iniciar Sesión", no_close=True, no_collapse=True, no_resize=True, width=281, height=155, pos=(620,300)):
     dpg.add_text("Usuario")
     dpg.add_input_text(tag="user", hint="Usuario", width=265)
     dpg.add_text("Contraseña")
@@ -49,11 +84,11 @@ with dpg.window(tag="login_window", label="Iniciar Sesión", no_close=True, no_c
     with dpg.group(horizontal=True):
         dpg.add_button(label="Iniciar Sesión", track_offset=0.5, callback=iniciar_sesion, pos=(30, 125))
         dpg.add_spacer(width=7)
-        dpg.add_button(label="Registrar", track_offset=0.5, callback=iniciar_sesion)
+        dpg.add_button(label="Registrar", track_offset=0.5, callback=registro_cuenta)
 
-dpg.create_viewport(title='Agenda Digital', width=500, height=300)
+dpg.create_viewport(title='Agenda Digital', width=1000, height=1000)
 dpg.setup_dearpygui()
 dpg.show_viewport()
-#dpg.set_primary_window("Primary Window", True)
+dpg.maximize_viewport()
 dpg.start_dearpygui()
 dpg.destroy_context()
